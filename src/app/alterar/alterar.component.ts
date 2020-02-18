b57fd3d00d21a8cdfabd44df228595080a0feaad
Tarik Lopes <tarikhero@gmail.com>
2020-02-18 17:42:24 -0300

    Add files via upload

---
+++ src/app/alterar/alterar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alterar',
+  templateUrl: './alterar.component.html',
+  styleUrls: ['./alterar.component.sass']
+})
+export class AlterarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
